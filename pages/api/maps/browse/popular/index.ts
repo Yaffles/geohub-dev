5d342013dd1b9bafc92b06913b135beef158257f
Yaffles <89337312+Yaffles@users.noreply.github.com>
2023-10-20 22:37:31 +1100

    Update index.ts

--- pages/api/maps/browse/popular/index.ts
+++ pages/api/maps/browse/popular/index.ts
@@ -8,16 +8,19 @@ import { collections, dbConnect, throwError } from '@backend/utils'
 console.log("fourth");
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("test");
+  console.log("5");
   try {
     await dbConnect()
 
     // HALP -> Sort maps by most liked -> going to need to query the mapLikes collection
+    console.log("6");
     const countQuery = req.query.count as string
     const mapCount = Number(countQuery)
     const mapId = req.query.mapId as string
+    console.log("7");
 
     if (req.method === 'GET') {
+      console.log("8");
       const maps = await collections.maps
         ?.find({
           _id: { $ne: new ObjectId(mapId) },
@@ -28,14 +31,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         .toArray()
 
       if (!maps) {
+        console.log("9");
         return throwError(res, 400, 'Failed to get popular maps')
       }
-
+      console.log("10");
       res.status(200).send(maps)
     } else {
+      console.log("11");
       res.status(405).end(`Method ${req.method} Not Allowed`)
     }
   } catch (err) {
+    console.log("12");
     console.error(err)
     res.status(500).json({ success: false })
   }
